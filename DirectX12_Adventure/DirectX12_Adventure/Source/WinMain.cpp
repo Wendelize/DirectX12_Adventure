92c5b5f71ec5c566ba877e315add6529fe9809b7
Vendela Eklund <TOXIC_FROG_KARLSKRONA>
2024-05-10 22:15:31 +0200

    Restructure WinMain script, seperating into regions and functions.

--- DirectX12_Adventure/DirectX12_Adventure/Source/WinMain.cpp
+++ DirectX12_Adventure/DirectX12_Adventure/Source/WinMain.cpp
@@ -1,26 +1,64 @@
+#pragma region Includes
 #include "PreCompiledHeaders.h"
+#pragma endregion
+
+///////////////////////////////////////////////////
+// GLOBAL VARIABLES
+///////////////////////////////////////////////////
+#pragma region GlobalVariables
 
 WCHAR WindowClass[MAX_NAME_SIZE];
 WCHAR WindowTitle[MAX_NAME_SIZE];
-
 INT WindowWidth;
 INT WindowHeight;
-
 HICON hIcon;
+#pragma endregion
 
-LRESULT CALLBACK WindowProcess(HWND hWnd, UINT msg, WPARAM wparam, LPARAM lparam) 
+
+///////////////////////////////////////////////////
+// FUNCTION DECLARATIONS
+///////////////////////////////////////////////////
+#pragma region FunctionDeclerations
+
+LRESULT CALLBACK WindowProcess(HWND hWnd, UINT msg, WPARAM wparam, LPARAM lparam);
+VOID InitializeVariables();
+VOID CreateWindowClass();
+VOID InitializeWindow();
+VOID MessageLoop();
+#pragma endregion
+
+
+///////////////////////////////////////////////////
+// MAIN LOOP
+///////////////////////////////////////////////////
+int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
+{
+	InitializeVariables();
+	CreateWindowClass();
+	InitializeWindow();
+	MessageLoop();
+	
+	return 0;
+}
+
+////////////////////////////////////////////////
+// FUNCTIONS
+////////////////////////////////////////////////
+#pragma region Functions
+
+LRESULT CALLBACK WindowProcess(HWND hWnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	switch (msg) {
 		// DESTROY WINDOW
-		case WM_DESTROY: 
-			PostQuitMessage(0); 
-			break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
 	}
 
 	return DefWindowProc(hWnd, msg, wparam, lparam);
 }
 
-int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
+VOID InitializeVariables() 
 {
 	// Init Global Variables
 	LoadString(HInstance(), IDS_GAMENAME, WindowTitle, MAX_NAME_SIZE);
@@ -29,31 +67,32 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 	WindowWidth = 1200;
 	WindowHeight = 800;
 	hIcon = LoadIcon(HInstance(), MAKEINTRESOURCE(IDI_MAINICON));
+}
 
+VOID CreateWindowClass() 
+{
 	// Create a Window Class
 	WNDCLASSEX window;
 
 	window.cbSize = sizeof(WNDCLASSEX);
 	window.style = CS_HREDRAW | CS_VREDRAW; // Horizontal/Vertical EDraw
-	window.cbClsExtra = 0;	
+	window.cbClsExtra = 0;
 	window.cbWndExtra = 0;
-
 	window.hCursor = LoadCursor(nullptr, IDC_ARROW);
 	window.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
-
 	window.hIcon = hIcon;
 	window.hIconSm = hIcon;
-
 	window.lpszClassName = WindowClass;
 	window.lpszMenuName = nullptr;
-
 	window.hInstance = HInstance();
-
 	window.lpfnWndProc = WindowProcess;
 
 	RegisterClassEx(&window);
+}
 
-	// Create and Display Window
+VOID InitializeWindow() 
+{
+	// Init and Display Window
 	HWND hWnd = CreateWindow(
 		WindowClass,
 		WindowTitle,
@@ -64,18 +103,21 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 		WindowHeight,
 		nullptr,			// Parent Window
 		nullptr,			// Menu
-		HInstance(),		
+		HInstance(),
 		nullptr);			// Something about taking instructions 
 
 	if (!hWnd)
 	{
 		// WINDOW ERROR
 		MessageBox(0, L"Failed to create window!", 0, 0);
-		return 0;
+		PostQuitMessage(0);
 	}
 
 	ShowWindow(hWnd, SW_SHOW);
+}
 
+VOID MessageLoop()
+{
 	// Listen for Message events 
 	MSG msg = { 0 };
 	while (msg.message != WM_QUIT)
@@ -88,6 +130,6 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 		}
 		// Add Game-Loop
 	}
-
-	return 0;
 }
+
+#pragma endregion
